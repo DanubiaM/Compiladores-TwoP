bcea7e995114e0623adbdbd7364c7b5ddd734bd7
DanubiaM <dubamacedoo@gmail.com>
2021-07-01 19:28:44 -0400

    feat: implementado analisador sintatico no cli

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+
 import argparse
 
 from lexico import Analisador_Lexico
 from sintatico import Analisador_Sintatico
-
+from semantico import Analisador_Semantico
 #analisador_lexico = Analisador_Lexico("C:\\Users\\danub\\OneDrive\\Área de Trabalho\\git\\Compiladores-TwoP\\arquivo_fonte.fon")
 
 #Método responsavel por retornar a lista de tokens.
@@ -22,6 +23,12 @@ def _parser(args):
 def lexical_analyzer(args):
   lista_tokens = list_tokens(args)   
   print_table_tokens(lista_tokens)
+
+#Metodo do analisador semântico
+def semantic_analyzer(args):
+  lista_tokens = list_tokens(args)
+  analisador_semantico = Analisador_Semantico(lista_tokens)
+  analisador_semantico.inicia_analise()
 
 #Impressao da tabela de tokens
 def print_table_tokens(list_tokens):
@@ -47,11 +54,15 @@ if __name__ == '__main__':
   parser.add_argument('-tudo', metavar="tudo", help="exibe todas as listagens do compilador")
   parser.add_argument('-lt', metavar="listtokens", help="exibe a listagem dos Tokens")
   parser.add_argument('-ls', metavar="parser", help="exibe o LOG do analisador sintático")
+  parser.add_argument('-lse', metavar="semanticanalysis", help="exibe o LOG do analisador semântico")
 
   args = parser.parse_args()  
   if args.tudo:
     all(args.tudo)    
   elif args.lt:
-    lexical_analyzer(args.lt)
+    lexical_analyzer(args.lt)  
   elif args.ls:
     _parser(args.ls)
+  elif args.lse:
+    semantic_analyzer(args.lse)
+  
