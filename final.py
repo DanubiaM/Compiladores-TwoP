424b08d1f03e0b8b096e62a2c3d80e08b1146951
DanubiaM <dubamacedoo@gmail.com>
2021-07-19 20:19:17 -0400

    feat: add struct condition partially finished

--- final.py
+++ final.py
@@ -8,7 +8,11 @@ class Codigo_Final:
         self.variaveis = []
         self.addr_var = []
         self.cont_string = 0
-        self.cont_label = 0 
+        self.cont_label = 0
+        self.label_if= 0 
+        self.label_endif = 0
+        self.label_else = 0
+        self.stack_label = []
         self.codigo_intermediario = []
         self._arquivo = open(cod_intermediario, "r")
 
@@ -21,7 +25,7 @@ class Codigo_Final:
         self.obter_tabela_variaveis()           #Obtem as variaveis 
         self.getAddr_var()                      #Cria endereços para as variaveis
         self.codigo_final.append(".global main")
-        self.codigo_final.append("{0}       @empilhamento endereco de retorno".format("main:\n push{ip, lr}"))
+        self.codigo_final.append("{0}       @empilhamento endereco de retorno".format("main:\npush {ip, lr}"))
         
         #self.codigo_final.append("main:\n push{ip, lr} '{0}".format("@empilhamaento endereço de retorno"))
         
@@ -33,38 +37,76 @@ class Codigo_Final:
                     self.codigo_final.append("{0}       @load addres of variable".format("LDR R1, ="+line.split()[1]))
                     self.codigo_final.append("BL scanf  @call function for read")                    
             elif line.split()[0] == 'escreva':
+                
+                print(line.split()[1])
                 self.codigo_final.append("{0}           @load addres of pattern number".format("LDR R0, ="+self.getAddr_var(line.split()[1])))
-                self.codigo_final.append("BL print      @call function for write")
+                self.codigo_final.append("BL printf      @call function for write")
                     
             elif line.split()[0] == 'se':
-                label = self.getLabel()
-                label_else: self.getLabel()
-                if len(line.split()>4):
-                    print("Possui conector lógico AND e OU")                            
-                else:
-                    self.calcula_expressao(line.split()[1])   
-                    self.codigo_final.append(" MOV R1, R0")    
-                    self.calcula_expressao(line.split()[3])
-                    self.codigo_final.append(" MOV R2, R0")
-                    self.codigo_final.append("CMP R1, R2")
+                print(line.split())
+                self.label_if = self.getLabel()
+                self.label_else = self.getLabel() 
+                self.label_endif = self.getLabel()         
+                self.stack_label.append(self.label_endif) #Label for endif
+
+                self.calcula_expressao(line.split()[1])  
+                self.codigo_final.append("pop {R0}")   
+                self.codigo_final.append("MOV R1, R0")    
+                self.calcula_expressao(line.split()[3])
+                self.codigo_final.append("pop {R0}")   
+                self.codigo_final.append("MOV R2, R0")
+                self.codigo_final.append("CMP R1, R2")
+                i=0
                 
-                if(line.split()[2] == ">"):                    
+                if len(line.split()) >5:
+                    for item in line.split():
+                                                          
+                        if item == "&" or item ==  "|":
+                            
+                            self.getExpBool(line.split()[i-2],item)                                              
+                            self.calcula_expressao(line.split()[i+1]) 
+                            self.codigo_final.append("pop {R0}")     
+                            self.codigo_final.append("MOV R1, R0")    
+                            self.calcula_expressao(line.split()[i+3])
+                            self.codigo_final.append("pop {R0}")   
+                            self.codigo_final.append("MOV R2, R0")
+                            self.codigo_final.append("CMP R1, R2")
+                            self.getExpBool(line.split()[i+2],item)
+ 
+                        i+=1
+                    print(line.split()[i-5])
+                    if line.split()[i-5] == "|": self.codigo_final.append("B {0}      @label content else".format(self.label_else))
+    
+                else:                    
+                    self.getExpBool(line.split()[2])    
+                    
+                self.stack_label.append(self.label_else)
+                self.codigo_final.append("{0}:      @label content if".format(self.label_if))
+                
+                """if(line.split()[2] == ">"):                    
                     self.codigo_final.append("BLE {0}".format(label))
                     self.codigo_final.append("B {0}".format(label_else))
                 elif line.split()[2] == "<":
                     self.codigo_final.append("BLT {0}".format(label))
                     self.codigo_final.append("B {0}".format(label_else))
-                self.codigo_final.append("{0}:      @label content if".format(label))    
+                self.codigo_final.append("{0}:      @label content if".format(label))"""    
             elif line.split()[0] == 'fim_se':
-                print()     
+                self.codigo_final.append("{0}:      @label for endif".format(self.stack_label.pop()))
+  
             elif  line.split()[0] == 'senao':
-                print()       
+                self.codigo_final.append("B {0}      @jump for endif".format(self.label_endif))                
+                self.codigo_final.append("{0}:      @label for else".format(self.stack_label.pop()))
+            
+                  
             elif  line.split()[1] == '=':
                 
                 self.calcula_expressao(line.split(" ",2)[2].strip())
                 self.codigo_final.append("pop {R1}          @pops in R1")
                 self.codigo_final.append("LDR R0, ={0}      @load address".format(line.split()[0]))
                 self.codigo_final.append("STR R1, [R0]      @store  result")
+        self.codigo_final.append("pop {ip, pc}")
+        self.addr_var.append(".global printf")
+        self.addr_var.append(".global scanf")
 
         #print(self.codigo_final)   
         intermediary = [str(a) for a in self.codigo_final]
@@ -74,6 +116,20 @@ class Codigo_Final:
         #print(self.addr_var)
       
 
+    def getExpBool(self, conector,op = None ):
+        if op == "&" or op == None:
+            self.codigo_final.append("@AND OPERATION")
+            if conector == ">":
+                 self.codigo_final.append("BLT {0}      @case Val1<Val2".format(self.label_else))
+            elif conector == "<":
+                print("here")
+                self.codigo_final.append("BGT {0}       @case Va1>Val2".format(self.label_else))
+        elif op == "|":
+            self.codigo_final.append("@OR OPERATION")
+            if conector == "<":
+                self.codigo_final.append("BLT {0}      @case Val1<Val2".format(self.label_if))
+            elif  conector == ">":
+                self.codigo_final.append("BGT {0}       @case Va1>Val2".format(self.label_if))
 
 
              
@@ -89,17 +145,19 @@ class Codigo_Final:
         #É chamado apenas uma vez no inicio, cria todos as variaveis
         if atr == None:
             self.addr_var.append(".data")
-            self.addr_var.append(".baling 8")
+            self.addr_var.append(".balign 8")
             self.addr_var.append("format: .asciz \"%d\"")
             #print (self.variaveis)
             for var in self.variaveis:                
-                self.addr_var.append(".baling 8")
+                self.addr_var.append(".balign 8")
                 self.addr_var.append("{0}: .word 0".format(var.strip()))
         #Chamado para printar, caso tenha alguma string do tipo "text"
-        elif re.search("^\"", atr):            
+        elif re.search("^\"", atr):
+            #print(atr)            
             self.cont_string += 1
-            self.addr_var.append(".baling 8")
+            self.addr_var.append(".balign 8")
             self.addr_var.append("string_{0}: .asciz {1}".format(self.cont_string,atr))
+            
 
             return "string_"+str(self.cont_string)
         #caso seja uma atribuicao ja declarada
@@ -108,7 +166,7 @@ class Codigo_Final:
 
     def getLabel(self):
         self.cont_label +=1
-        return "L{0}:.format(self.cont_label)"
+        return "L{0}".format(self.cont_label)
 
     def calcula_expressao(self, expressao):
         
@@ -143,17 +201,20 @@ class Codigo_Final:
                     self.codigo_final.append("MUL R0, R0, R1    @multiplication operation")
                     self.codigo_final.append("push {R0}         @stack result content")
                 elif atr.strip() == "/":   
-                    self.codigo_final.append("pop {R1}          @pops in R1")
-                    self.codigo_final.append("pop {R2}          @pops in R2")
+                    self.codigo_final.append("pop {R2}          @pops in R1")
+                    self.codigo_final.append("pop {R1}          @pops in R2")
                     self.codigo_final.append("MOV R0, #0        @init variable for resultable")
                     self.codigo_final.append("_division:        @create label")
-                    self.codigo_final.append("SUB R1, R1, R2    @subtraction operation")
+                    self.codigo_final.append("SUBS R1, R1, R2    @subtraction operation")
                     self.codigo_final.append("ADD R0, R0,#1     @result division")
-                    self.codigo_final.append("BHI _division:    @jump case R1>R@")
-                    self.codigo_final.append("push {R0}          @stack result content")
+                    self.codigo_final.append("BHI _division     @jump case R1>R2")
+                    self.codigo_final.append("push {R0}         @stack result content")
         else:
             if expressao.isdigit():           
                 self.codigo_final.append("MOV R0, #{0}      @load addres for variable".format(expressao))
+                #Ultima alteração
+                self.codigo_final.append("push {R0}         @stack result content")
+
             else:
                 self.codigo_final.append("LDR R0, ={0}      @load addres for variable".format(expressao))
                 self.codigo_final.append("LDR R0, [R0]      @load data of variable")
